353e70acdd0347e41005f940273fc1f3f91d7ff3
IkramAmh <ikramamhirrach2@gmail.com>
2023-06-01 16:45:33 -0700

    get_flags

--- main.h
+++ main.h
@@ -1,3 +1,4 @@
 int _printf(const char *format, ...);
 int print_unsigned(va_list types, char buffer[],
         int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
